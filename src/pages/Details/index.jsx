78f51277fe285b73a5f117c0a9c9804fefdf052b
bruno <brunoedua21@gmail.com>
2023-02-09 11:17:06 -0400

    create Button and Head component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,24 @@
-import { Container } from "./style.js"
+import { Container, Links } from "./styles.js"
 import { Button } from "../../components/Button"
-import { Header } from "../../components/Header/index.jsx"
+import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
 
 export function Details() {
 
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://rocketseat.com.br/</a>
+          </li>
+          <li>
+            <a href="#">https://rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
       <Button title="Voltar" />
     </Container>
   )
