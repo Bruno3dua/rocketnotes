41b78111233a4bca21230b199e14c52ecfc646e7
bruno <brunoedua21@gmail.com>
2023-02-09 11:55:45 -0400

    notes page complete

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,9 @@
-import { Container, Links } from "./styles.js"
+import { Container, Links, Content } from "./styles.js"
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
+import { Tags } from "../../components/Tags"
+import { ButtonText } from "../../components/ButtonText"
 
 export function Details() {
 
@@ -9,17 +11,37 @@ export function Details() {
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">https://rocketseat.com.br/</a>
-          </li>
-          <li>
-            <a href="#">https://rocketseat.com.br/</a>
-          </li>
-        </Links>
-      </Section>
-      <Button title="Voltar" />
+      <main>
+        <Content>
+
+          <ButtonText title="Excluir a nota" />
+
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi voluptatum necessitatibus
+            cumque magni accusamus voluptatibus? Odit quidem explicabo nulla. Necessitatibus architecto
+            nostrum ullam blanditiis voluptates debitis delectus eos quia illo!
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://rocketseat.com.br/</a></li>
+              <li><a href="#">https://rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tags title="express" />
+            <Tags title="node" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+
+      </main>
     </Container>
   )
 }
