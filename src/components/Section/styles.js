890b31bb877fb5598d7a026df12d2bb960fc8a21
bruno <brunoedua21@gmail.com>
2023-02-09 19:53:15 -0400

    finish homepage

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -6,6 +6,7 @@ export const Container = styled.section`
     > h2 {
         border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
         padding-bottom: 1.6rem;
+        margin-bottom: 2.4rem;
         
         color: ${({ theme }) => theme.COLORS.GRAY_100};
         font-size: 2rem;
