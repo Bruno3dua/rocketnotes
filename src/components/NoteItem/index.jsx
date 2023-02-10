680d8458af6e248a9ca34269cc0a738574996fe9
bruno <brunoedua21@gmail.com>
2023-02-10 18:52:19 -0400

    add new note page

---
+++ src/components/NoteItem/index.jsx
@@ -0,0 +1,23 @@
+import { Container } from "./styles";
+import { FiPlus, FiX } from "react-icons/fi"
+
+export function NoteItem({ isNew, value, onclick, ...rest }) {
+    return (
+        <Container isNew={isNew}>
+            <input
+                type="text"
+                readOnly={!isNew}
+                value={value}
+                {...rest}
+            />
+
+            <button
+                type="button"
+                onclick={onclick}
+                className={isNew ? "button-add" : "button-delete"}
+            >
+                {isNew ? <FiPlus /> : <FiX />}
+            </button>
+        </Container>
+    )
+}
